abfff7b96c0e594865ee0b2f8402b322e4c439a7
JRAH1234 <49672544+JRAH1234@users.noreply.github.com>
2021-08-06 16:24:25 +0100

    Added Application Insights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppInsightsService } from '@markpieszak/ng-application-insights';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private appInsightsService: AppInsightsService) {};
+
   title = 'hello-world-app';
+  testfn()
+  {
+    this.appInsightsService.trackEvent('Test trace message', { 'user': 'jp', 'cart': '1234' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationInsightsModule, AppInsightsService } from '@markpieszak/ng-application-insights';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApplicationInsightsModule.forRoot({
+      instrumentationKey: '4a74ff1c-d1e0-401b-98f8-d0e83526ebd5'
+    })
   ],
-  providers: [],
+  providers: [AppInsightsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
