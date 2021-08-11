cfe62365d3116724404451342123a1dff6ce17e3
JRAH1234 <49672544+JRAH1234@users.noreply.github.com>
2021-08-11 17:09:25 +0100

    AppID change

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+import { CommonModule } from '@angular/common';
+
+
 
 if (environment.production) {
   enableProdMode();
